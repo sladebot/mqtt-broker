0131f746bf8108320642fd301dfe027a911207f5
Souranil Sen <souranil@gmail.com>
2017-03-05 02:34:14 -0500

    Adding test pipeline
    * Adding standard and pretest hooks
    * standard will fix most of the js quality checks
    * Adding snazzy for formatting errors.

--- test/vegeta.js
+++ test/vegeta.js
@@ -1,15 +1,15 @@
-'use strict';
+'use strict'
 
-import { assert } from 'chai';
-import { Vegeta } from "../vegeta";
+import { assert } from 'chai'
+import { Vegeta } from '../vegeta'
 
 /**
  * Vegeta Rises !
  */
 describe('vegeta', () => {
-  describe("#export", () => {
-    it("should be importable", () => {
-      assert(typeof(Vegeta), "function");
+  describe('export', () => {
+    it('should be importable', () => {
+      assert(typeof (Vegeta), 'function')
     })
   })
 })
--- vegeta.js
+++ vegeta.js
@@ -1,5 +1,5 @@
-"use strict";
+'use strict'
 
-export default function Vegeta() {
-  
+export default function Vegeta () {
+
 }
