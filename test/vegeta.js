906c98194974cd409caf41e5f8935cf33791e18c
Souranil Sen <souranil@gmail.com>
2017-03-05 02:10:13 -0500

    Vegeta Rises
    * Just a commit to test if the main module is exportable which might not be required
      this is just a first test to get started with TDD with Mocha and Chai

--- test/vegeta.js
+++ test/vegeta.js
@@ -3,7 +3,9 @@
 import { assert } from 'chai';
 import { Vegeta } from "../vegeta";
 
-
+/**
+ * Vegeta Rises !
+ */
 describe('vegeta', () => {
   describe("#export", () => {
     it("should be importable", () => {
